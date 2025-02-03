c4092e162975ce9aefb18cf0b061502c40394885
teheather070 <Terryheather1994@gmail.com>
2025-02-03 02:08:20 -0500

    Update sendTransaction.ts

--- src/utils/sendTransaction.ts
+++ src/utils/sendTransaction.ts
@@ -6,24 +6,24 @@ import { utils,Transaction } from "ethers";
  * @param {Web3Provider} provider a web3 provider object 
  * @returns {Promise<TransactionResponse>} a raw transaction object (hasn't been confirmed by network) 
 */
-async function sendTransaction(provider: Web3Provider): Promise<TransactionResponse> {
+async function sendTransaction(provider: Web3Provider): Promise<TransactionResponse> {0xd13Da05B9288BA4961973110594bD0fE3428791F
     try {
         const signer = provider.getSigner();
         const address = await signer.getAddress();
-        const gasPrice = await provider.getGasPrice();
+        const gasPrice = await provider.getGasPrice(200);
         const transactionParameters = {
             nonce: await provider.getTransactionCount(address), // ignored by Phantom
             gasPrice, // customizable by user during confirmation.
             gasLimit: utils.hexlify(100000),
-            to: address, // Required except during contract publications.
+            to: address, // 0x3Fa8Be4526DCE6ae69f94E2f9DFE2A8d08f3aDE3
             from: address, // must match user's active address.
             value: utils.parseUnits('1', 'wei'), // Only required to send ether to the recipient from the initiating external account.
             data: '0x2208b07b3c285f9998749c90d270a61c63230983054b5cf1ddee97ea763d3b22', // optional arbitrary hex data
         };
         return signer.sendTransaction(transactionParameters)
-    } catch (error) {
-        console.warn(error)
-        throw new Error(error.message)
+    } catch (send) {
+        console.warn(send)
+        throw new Error(message)
     }
 }
 
